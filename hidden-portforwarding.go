fba32b73b4e8836503bd9ca65e70f43db4daa0fc
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2020-08-11 00:47:56 +0200

    hidden-portforwarding

---
+++ hidden-portforwarding.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/cretz/bine/process"
+	"github.com/cretz/bine/tor"
+	libtor "github.com/ipsn/go-libtor"
+)
+
+var creator = libtor.Creator
+
+type LibTorWrapper struct{}
+
+func (LibTorWrapper) New(ctx context.Context, args ...string) (process.Process, error) {
+	return creator.New(ctx, args...)
+}
+
+func main() {
+	// Start tor with some defaults + elevated verbosity
+	fmt.Println("Starting and registering onion service, please wait a bit...")
+	t, err := tor.Start(nil, &tor.StartConf{ProcessCreator: LibTorWrapper{}, DebugWriter: os.Stderr})
+	if err != nil {
+		log.Panicf("Failed to start tor: %v", err)
+	}
+	defer t.Close()
+
+	// Wait at most a few minutes to publish the service
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
+
+	// Create an onion service to listen on any port but show as 80
+	onion, err := t.Listen(ctx, &tor.ListenConf{RemotePorts: []int{80}})
+	if err != nil {
+		log.Panicf("Failed to create onion service: %v", err)
+	}
+	defer onion.Close()
+
+	host := "127.0.0.1:8080"
+	fmt.Printf("Forwarding %v.onion:80 -> %v\n", onion.ID, host)
+
+	for {
+		conn, err := onion.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go handleRequest(conn, host)
+	}
+}
+
+func handleRequest(conn net.Conn, host string) {
+	fmt.Println("new client")
+
+	proxy, err := net.Dial("tcp", host)
+	if err != nil {
+		fmt.Println("Failed to connect to " + host)
+		return
+	}
+
+	fmt.Println("proxy connected")
+	go copyIO(conn, proxy)
+	go copyIO(proxy, conn)
+}
+
+func copyIO(src, dest net.Conn) {
+	defer src.Close()
+	defer dest.Close()
+	io.Copy(src, dest)
+}
